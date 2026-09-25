7cb0d5898de5bb05bfaa62baa8e4fc202c181b9b
Denis <dexdenis3@gmail.com>
2026-09-25 14:57:25 +0300

    refactor: remove background gradient from Navbar link for simplified styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,7 +35,7 @@ const Navbar = () => {
     )}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
-          <Link href="/" className="text-xl font-bold bg-linear-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent">
+          <Link href="/" className="text-xl font-bold text-blue-400">
             Denis Murithi
           </Link>
 
